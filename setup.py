491fc6749e0776bc206b980673144229bc07eac8
Joel <joel.c@scene.community>
2018-11-28 15:45:04 +0000

    added a setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+packages = ['streamz4', 'streamz4.dataframe']
+
+tests = [p + '.tests' for p in packages]
+
+
+setup(name='streamz4',
+      version='0.0.1',
+      description='Streams',
+      url='https://github.com/keepexploring/streamz3',
+      maintainer='Matthew Rocklin',
+      license='BSD',
+      keywords='streams',
+      packages=packages + tests,
+      install_requires=[tornado, toolz, zict, six],
+      zip_safe=False)
